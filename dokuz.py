56be653b5af9767a44c0a36518743e18b2edfe23
omer <onc@uludag.edu.tr>
2022-04-25 12:05:12 +0300

    pygame init hazırlandı.

--- dokuz.py
+++ dokuz.py
@@ -1,2 +1,13 @@
+import sys
 import pygame as pg
 
+pg.init()
+size = 800,600
+ekran = pg.display.set_mode(size)
+while 1:
+    for event in pg.event.get():
+        if event.type == pg.QUIT: 
+            sys.exit()
+    ekran.fill((0,255,0))
+    pg.display.flip()
+    
